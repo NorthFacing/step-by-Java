20d90f0fd86bf10da293032bfd2aa8e58f67a6a6
Bob <0haizhu0@gmail.com>
2016-05-20 13:49:26 +0800

    栈的范例：括号匹配

--- CS-data-structure/src/main/java/com/book/book01/chapter04/bob/bracket/BracketDemo.java
+++ CS-data-structure/src/main/java/com/book/book01/chapter04/bob/bracket/BracketDemo.java
@@ -4,4 +4,9 @@ package com.book.book01.chapter04.bob.bracket;
  * Created by Bob on 2016/5/20.
  */
 public class BracketDemo {
+  public static void main(String[] args) {
+    String input = "asdf{asdf}asdf{sdf[(asdf0)](}asdf";
+    BracketImpl bracket = new BracketImpl();
+    bracket.bracketCheck(input);
+  }
 }
--- CS-data-structure/src/main/java/com/book/book01/chapter04/bob/bracket/BracketImpl.java
+++ CS-data-structure/src/main/java/com/book/book01/chapter04/bob/bracket/BracketImpl.java
@@ -1,7 +1,47 @@
 package com.book.book01.chapter04.bob.bracket;
 
+import com.book.book01.chapter04.bob.stack.StackImpl;
+
 /**
  * Created by Bob on 2016/5/9.
  */
 public class BracketImpl {
+
+  public void bracketCheck(String input) {
+
+    int length = input.length();
+
+    StackImpl stack = new StackImpl(length);
+
+    for (int i = 0; i < length; i++) {
+      char ch = input.charAt(i);
+      switch (ch) {
+        case '{':
+        case '[':
+        case '(':
+          stack.push(ch);
+          break;
+        case ')':
+        case ']':
+        case '}':
+          if (stack.isEmpty()) {
+            System.err.println("缺失左括号");
+          } else {
+            char pop = (char) stack.pop();
+            if ((ch == '}' && pop != '{')
+                    || (ch == ']' && pop != '[')
+                    || (ch == ')' && pop != '(')) {
+              System.err.println("括号不匹配");
+            }
+          }
+          break;
+        default:
+          break;
+      }// end switch
+    }// end for
+    if(!stack.isEmpty()){
+      System.err.println("缺失右括号");
+    }
+  }
+
 }
