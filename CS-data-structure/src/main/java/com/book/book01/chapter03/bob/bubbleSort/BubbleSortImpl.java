fb05b3be40bf397b39616c0a3829ac16c035d42e
Bob <0haizhu0@gmail.com>
2016-06-21 16:45:36 +0800

    数据结构 - 变量提取都外边，跟其他简单排序统一

--- CS-data-structure/src/main/java/com/book/book01/chapter03/bob/bubbleSort/BubbleSortImpl.java
+++ CS-data-structure/src/main/java/com/book/book01/chapter03/bob/bubbleSort/BubbleSortImpl.java
@@ -20,8 +20,9 @@ public class BubbleSortImpl implements BubbleSort {
 
   @Override
   public void bubbleSort() {
-    for (int out = len - 1; out > 1; out--) { // 只有两项的时候就不用比较了，但是即便携程out>0，也没有问题
-      for (int in = 0; in < out; in++) {
+    int out, in;
+    for (out = len - 1; out > 1; out--) { // 只有两项的时候就不用比较了，但是即便携程out>0，也没有问题
+      for (in = 0; in < out; in++) {
         if (arr[in] > arr[in + 1]) // out of order ?
           swap(in, in + 1);
       }
