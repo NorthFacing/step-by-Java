4af81918260c64f664ab97b5d36838a36d7419db
Bob <0haizhu0@gmail.com>
2016-06-21 14:51:28 +0800

    数据结构 - 简单配许：冒泡排序

---
+++ CS-data-structure/src/main/java/com/book/book01/chapter03/bob/bubbleSort/BubbleSort.java
@@ -0,0 +1,14 @@
+package com.book.book01.chapter03.bob.bubbleSort;
+
+/**
+ * Created by Bob on 2016/6/21.
+ */
+public interface BubbleSort {
+
+  void insert(long value);
+
+  void bubbleSort();
+
+  void display();
+
+}
---
+++ CS-data-structure/src/main/java/com/book/book01/chapter03/bob/bubbleSort/BubbleSortImpl.java
@@ -0,0 +1,46 @@
+package com.book.book01.chapter03.bob.bubbleSort;
+
+/**
+ * Created by Bob on 2016/6/21.
+ */
+public class BubbleSortImpl implements BubbleSort {
+
+  private long[] arr;
+  private int len;
+
+  public BubbleSortImpl(int maxSize) {
+    arr = new long[maxSize];
+    this.len = 0;
+  }
+
+  @Override
+  public void insert(long value) {
+    arr[len++] = value;
+  }
+
+  @Override
+  public void bubbleSort() {
+    for (int out = len - 1; out > 1; out--) { // 只有两项的时候就不用比较了，但是即便携程out>0，也没有问题
+      display();
+      for (int in = 0; in < out; in++) {
+        if (arr[in] > arr[in + 1]) // out of order ?
+          swap(in, in + 1);
+      }
+    }
+  }
+
+  @Override
+  public void display() {
+    for (int i = 0; i < len; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+  }
+
+  private void swap(int i, int j) {
+    long temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+}
