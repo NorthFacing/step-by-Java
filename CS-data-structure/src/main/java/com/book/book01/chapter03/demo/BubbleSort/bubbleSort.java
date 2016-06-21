d8cf580845f1b72f17e798b756dbea24df775f49
Bob <0haizhu0@gmail.com>
2016-06-21 16:44:48 +0800

    数据结构 - 去掉测试语句

--- CS-data-structure/src/main/java/com/book/book01/chapter03/demo/BubbleSort/bubbleSort.java
+++ CS-data-structure/src/main/java/com/book/book01/chapter03/demo/BubbleSort/bubbleSort.java
@@ -33,9 +33,7 @@ class ArrayBub {
   //--------------------------------------------------------------
   public void bubbleSort() {
     int out, in;
-
     for (out = nElems - 1; out > 1; out--)   // outer loop (backward)
-      display();
       for (in = 0; in < out; in++)        // inner loop (forward)
         if (a[in] > a[in + 1])       // out of order?
           swap(in, in + 1);          // swap them
