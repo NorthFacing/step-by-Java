14c8e4c1cdd3a9e6781826ae331546e8c6f1cfc6
Bob <0haizhu0@gmail.com>
2017-12-16 22:22:07 +0800

    ListView

---
+++ java-JavaFx/src/main/java/com/adolphor/thenewboston/tutor16_TreeView/Main.java
@@ -0,0 +1,60 @@
+package com.adolphor.thenewboston.tutor16_TreeView;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	Stage window;
+	TreeView<String> tree;
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		window = primaryStage;
+		window.setTitle("example");
+
+		TreeItem<String> root = new TreeItem<>();
+		root.setExpanded(true);
+
+		// branch A
+		TreeItem<String> branch_A = makeBranch("Branch-A", root);
+		makeBranch("A1", branch_A);
+		makeBranch("A2", branch_A);
+		makeBranch("A3", branch_A);
+
+		// branch B
+		TreeItem<String> branch_B = makeBranch("Branch-B", root);
+		makeBranch("B1", branch_B);
+		makeBranch("B2", branch_B);
+
+		tree = new TreeView<>(root);
+		tree.setShowRoot(false);
+		tree.getSelectionModel().selectedItemProperty()
+			.addListener((v, oldValue, newValue) -> {
+				if (newValue != null)
+					System.out.println(newValue.getValue());
+			});
+
+		StackPane layout = new StackPane();
+		layout.getChildren().add(tree);
+		Scene scene = new Scene(layout, 500, 300);
+		window.setScene(scene);
+		window.show();
+	}
+
+	private TreeItem<String> makeBranch(String title, TreeItem<String> parent) {
+		TreeItem<String> treeItem = new TreeItem<>(title);
+		treeItem.setExpanded(true);
+		parent.getChildren().add(treeItem);
+		return treeItem;
+	}
+
+}
