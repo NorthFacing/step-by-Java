2f4a547bdbb3e7314884d1ac12a45b8d485277bb
Bob <0haizhu0@gmail.com>
2017-12-19 20:59:52 +0800

    FXML

---
+++ java-JavaFx/src/main/java/com/adolphor/thenewboston/tutor31_FXML/Controller.java
@@ -0,0 +1,21 @@
+package com.adolphor.thenewboston.tutor31_FXML;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+	@FXML
+	public void handleButtonClicked(){
+		System.out.println("handleButtonClicked...");
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		System.out.println("View is now loaded!");
+	}
+
+}
